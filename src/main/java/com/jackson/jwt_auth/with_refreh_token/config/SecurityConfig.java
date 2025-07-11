2d10e4116992e4c92684319b3584e4c5fcb869a7
Win Min Thant <jackson14042004@gmail.com>
2025-07-11 15:05:51 +0630

    added security chain method

--- src/main/java/com/jackson/jwt_auth/with_refreh_token/config/SecurityConfig.java
+++ src/main/java/com/jackson/jwt_auth/with_refreh_token/config/SecurityConfig.java
@@ -4,13 +4,45 @@
  * Version      : 	
  * History  :	
  * *************************************************************/
-package com.jackson.jwt_auth.with_refreh_token.config;/**
+package com.jackson.jwt_auth.with_refreh_token.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
  * SecurityConfig Class.
  * <p>
  * </p>
- * 
+ *
  * @author
  */
 
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                                           .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(server -> server
+                        .jwt(Customizer.withDefaults())
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
+                .build();
+    }
+
+    
+
 }
