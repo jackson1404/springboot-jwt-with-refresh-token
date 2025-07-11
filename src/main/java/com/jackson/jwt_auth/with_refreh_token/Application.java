a0647575bf19d6476ff1e7611b963e7575ea5f44
Win Min Thant <jackson14042004@gmail.com>
2025-07-11 10:33:18 +0630

    first commit

---
+++ src/main/java/com/jackson/jwt_auth/with_refreh_token/Application.java
@@ -0,0 +1,13 @@
+package com.jackson.jwt_auth.with_refreh_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackson/jwt_auth/with_refreh_token/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson.jwt_auth.with_refreh_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
