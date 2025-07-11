ddf7a3b981c36ef35930c326d132502bf8c96518
Win Min Thant <jackson14042004@gmail.com>
2025-07-11 14:01:42 +0630

    added jpa config

---
+++ src/main/java/com/jackson/jwt_auth/with_refreh_token/config/JpaConfig.java
@@ -0,0 +1,23 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.jackson.jwt_auth.with_refreh_token.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * JpaConfig Class.
+ * <p>
+ * </p>
+ *
+ * @author
+ */
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
