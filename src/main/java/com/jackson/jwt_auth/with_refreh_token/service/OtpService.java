08360e54d001ed3e0681678c6c26abc50f4dc14d
jackson1404 <jackson14042004@gmail.com>
2025-07-13 14:30:40 +0630

    added otp service for generate and get cache key

---
+++ src/main/java/com/jackson/jwt_auth/with_refreh_token/service/OtpService.java
@@ -0,0 +1,43 @@
+package com.jackson.jwt_auth.with_refreh_token.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class OtpService {
+
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public String generateAndStoreOtp(final Long userId){
+
+        final var otp = generateOtp("ABCDEFG123456789", 10);
+        final var cacheKey = getCacheKey(userId);
+
+        redisTemplate.opsForValue().set(cacheKey, otp, Duration.ofMinutes(5));
+
+        return otp;
+    }
+
+
+
+    public String getCacheKey(final Long userId){
+        return "otp:%s".formatted(userId);
+    }
+
+    public String generateOtp(String character, int length){
+        StringBuilder otp = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = SECURE_RANDOM.nextInt(character.length());
+            otp.append(character.charAt(index));
+        }
+        return otp.toString();
+    }
+
+}
