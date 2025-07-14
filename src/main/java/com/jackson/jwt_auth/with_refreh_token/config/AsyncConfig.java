4110f8a479fe03bcbf663be3c684cbc79962a9e8
Win Min Thant <jackson14042004@gmail.com>
2025-07-14 09:13:43 +0630

    added config for email verify service class

---
+++ src/main/java/com/jackson/jwt_auth/with_refreh_token/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.jackson.jwt_auth.with_refreh_token.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * AsyncConfig Class.
+ * <p>
+ * </p>
+ *
+ * @author
+ */
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
---
+++ src/main/java/com/jackson/jwt_auth/with_refreh_token/service/EmailVerificationService.java
@@ -0,0 +1,24 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.jackson.jwt_auth.with_refreh_token.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * EmailVerificationService Class.
+ * <p>
+ * </p>
+ *
+ * @author
+ */
+
+@Service
+public class EmailVerificationService {
+
+    
+
+}
